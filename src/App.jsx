6d3234a4006b3bd64110639c417e7b908dc15e67
Dyzer4 <dyegodasilvaroque86@gmail.com>
2025-10-27 16:49:50 -0300

    feat add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Lightning from './components/Lightning/Lightning';
+import ElectricBorder from './components/ElectricBorder/ElectricBorder'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="background">
+          <Lightning
+            hue={220}
+            xOffset={0}
+            speed={1}
+            intensity={0.5}
+            size={0.5}
+          />
+          <Lightning
+            hue={220}
+            xOffset={0}
+            speed={100}
+            intensity={0.1}
+            size={0.5}
+          />
+        </div>
+        
+          <ElectricBorder
+            color="#7df9ff"
+            speed={1}
+            chaos={0.5}
+            thickness={2}
+            style={{ borderRadius: 16, position: 'absolute',  }}
+          >
+            <div>
+              <p style={{ margin: '6px 0 0', opacity: 0.8 }}>
+                A glowing, animated border wrapper.
+              </p>
+            </div>
+          </ElectricBorder>
+      </main>
     </>
   )
 }
