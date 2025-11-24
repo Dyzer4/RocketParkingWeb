efe23347cac59210b4e1853126020f06545b8b14
Dyzer4 <dyegodasilvaroque86@gmail.com>
2025-11-24 14:58:39 -0300

    fix: scroll bar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import './style.css'
 export const Header = () => {
     return (
         <header>
-            <h1>Rocket Parking</h1>
+            <h1>Thunder Parking</h1>
         </header>
     )
 } 
